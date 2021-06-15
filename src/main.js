68dc70ccaf26bc1599d334dd7f32dcde5a531cb2
Gigena-Cod <nahuel@leadboxhq.com>
2021-06-15 11:36:10 -0300

    Version Beta

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Checkbox from 'primevue/checkbox';
+import InputNumber from 'primevue/inputnumber';
+import Button from 'primevue/button';
+import Message from 'primevue/message';
+
+import 'primevue/resources/themes/saga-blue/theme.css';    
+import 'primevue/resources/primevue.min.css';   
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('Checkbox', Checkbox);
+app.component('Button', Button);
+app.component('Message', Message);
+app.component('InputNumber', InputNumber);
+app.mount("#app");
+
